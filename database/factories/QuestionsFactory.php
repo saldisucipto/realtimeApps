e7804c477f1c9d9b436b324300d619057f44e98c
Saldi Sucipto <saldisucipto@gmail.com>
2020-10-06 17:01:04 +0700

    DB Seed And Factory

--- database/factories/QuestionsFactory.php
+++ database/factories/QuestionsFactory.php
@@ -6,7 +6,18 @@ use App\Model\Questions;
 use Faker\Generator as Faker;
 
 $factory->define(Questions::class, function (Faker $faker) {
+    $title = $faker->sentence;
     return [
-        //
+        //make faker 
+        'title' => $title, 
+        'slug' => str_slug($title),
+        'body' => $faker->text(),
+        'category_id' => function(){
+            return App\Model\Category::all()->random();
+        },
+        'user_id' => function(){
+            return \App\User::all()->random();
+        }
+
     ];
 });
